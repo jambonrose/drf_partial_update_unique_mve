e1e9c1ca34cbc0187e842eebad2c1605a17a5a0d
Andrew Pinkham <code@andrewsforge.com>
2018-11-28 10:46:27 -0500

    ✨ Define data models for MVE

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+"""Data models for Blog; actually a MVE for DRF bug"""
+from datetime import date
 
-# Create your models here.
+from django.db.models import CharField, DateField, Model, SlugField
+
+
+class Post(Model):
+    """Simple Post object for MVE purposes
+
+    Demonstrates working ModelSerializer usage
+    """
+
+    title = CharField(max_length=63)
+    slug = SlugField(max_length=63)
+    pub_date = DateField(default=date.today)
+
+
+class UniquePost(Model):
+    """Post object with a unique constraint
+
+    Demonstrates failing ModelSerializer usage
+    """
+
+    title = CharField(max_length=63)
+    slug = SlugField(max_length=63, unique_for_month="pub_date")
+    pub_date = DateField(default=date.today)
