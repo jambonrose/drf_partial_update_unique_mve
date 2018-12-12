7a5591bde68e2a33ce040ae7b23913755e13c301
Andrew Pinkham <code@andrewsforge.com>
2018-12-12 16:26:10 +0100

    🔧 Add django-extensions to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party apps
+    "django_extensions",
     "rest_framework",
     # local apps
     "blog.apps.BlogConfig",
